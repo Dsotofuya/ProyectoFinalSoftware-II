ebcc0ce51c2912bdee015cd70181451384ec50a7
Dsotofuya <dsotofuya@gmail.com>
2022-02-19 17:31:47 -0500

    Prueba inicial Js

---
+++ pruebas.js
@@ -0,0 +1,14 @@
+let varText = "xd";
+let varNum = 123;
+
+// let variable local
+// var variable global
+
+// Operadores
+// que sea igual ==
+// que sea igual y del mismo tipo ===
+// Distinto !=
+
+console.log("Variable: " + variable);
+
+const constante = "constante";
