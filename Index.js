b320e9e34eac231b96375e878e61735b0729c4f4
Dsotofuya <dsotofuya@gmail.com>
2022-02-26 21:31:13 -0500

    Pruebas

--- Index.js
+++ Index.js
@@ -1,4 +1,41 @@
-const pruebas = require('./pruebas.js');
+const express = require("express");
 
-pruebas.print("asdasc")
-pruebas.print2("jmmm")
+var server = express();
+server.listen(4445);
+server.use(express.json());
+server.use((req, res, next) => {
+  console.log("Pasa");
+  next();
+});
+
+// server.all("/registro", (req, res, next) => {
+//   console.log("para todas las rutas /registro muestra en consola esto xd.");
+//   next();
+// });
+
+server.get("/registro/:user", (req, res) => {
+  res.send("Testeo inicial del server y pagina inicial");
+});
+
+server.post("/registro/:user", (req, res) => {
+  console.log(req.params);
+  //console.log(req.body);
+  res.send("peticion post, acá se cargará el registro de la pagina");
+});
+
+// server.delete("/registro/:user", (req, res) => {
+//   //console.log(`User ${req.params.user} borrado`);
+//   //console.log(req.body);
+//   res.send(`User ${req.params.user} borrado`);
+//   //res.send('peticion post, acá se cargará el registro de la pagina');
+// });
+
+server.put("/registro/:user", (req, res) => {
+  res.send("peticion de actualizar, acá se cargará el registro de la pagina");
+});
+
+server.delete("/registro/:user", (req, res) => {
+  res.send("petición de borrar, acá se cargará el registro de la pagina");
+});
+
+server.use(express.static('webapp'));
