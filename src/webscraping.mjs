87224918039b32dddfecb57b3c20596b16a277db
Dsotofuya <dsotofuya@gmail.com>
2022-03-07 10:11:52 -0500

    Webscraping de steam Funcionando

--- src/webscraping.mjs
+++ src/webscraping.mjs
@@ -2,38 +2,25 @@
 import fetch from "node-fetch";
 
 // Se saca los datos en formato JSON del api de steam
-const response = await fetch(
-  "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-);
-const data = await response.json();
-// Se filtran los datos apartir del JSON recibido
-const appList = data.applist.apps;
-
-//URL de la segunda api para obtener datos detallados de un juego especifico
-const gameInfo = "https://store.steampowered.com/api/appdetails?appids=";
-
-//Funcion asincrona para obtener los datos de un juego apartir de su nombre en concreto
-// async function getGame(gameName) {
-//   appList.forEach( async function (app) {
-//     if (app.name === gameName) {
-//       console.log(app.appid, ": ", app.name, " has been requested");
-//       return app.appid;
-//     } else {
-//       console.log("error");
-//     }
-//   });
-// }
-
 async function getGame(gameName) {
-  const { appid } = appList.find(app => app.name === gameName);
+  const response = await fetch(
+    "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+  );
+  const data = await response.json();
+  // Se filtran los datos apartir del JSON recibido
+  const appList = data.applist.apps;
+  //Funcion asincrona para obtener los datos de un juego apartir de su nombre en concreto
+  const { appid } = appList.find((app) => app.name === gameName);
   return appid;
 }
 
 //Funcion asincrona para objetener los datos especificos de un juego apartir de su id, además de retornar un objeto con los valores necesitados
 async function getGameInfo(gameId) {
+  //URL de la segunda api para obtener datos detallados de un juego especifico
+  const gameInfo = "https://store.steampowered.com/api/appdetails?appids=";
   let gameResponse = await fetch(gameInfo + gameId);
   let game = await gameResponse.json();
-   return {
+  return {
     name: game[gameId].data.name,
     genres: game[gameId].data.genres,
     header_image: game[gameId].data.header_image,
@@ -43,13 +30,4 @@ async function getGameInfo(gameId) {
   };
 }
 
-//console.log(getGame("Beast Master"));
-//  console.log(getGame("Halo Infinite (Campaign)"));
-// console.log(await getGameInfo(getGame("Halo Infinite (Campaign)")));
-// const gameidA = getGame("Halo Infinite (Campaign)");
-// console.log(gameidA.resolve());
-// console.log(await getGameInfo(gameidA));
-console.log(await getGameInfo(1708091));
-
-
-// console.log(getGame("Halo Infinite (Campaign)"));
+console.log(await getGameInfo(await getGame("Halo Infinite (Campaign)")));
