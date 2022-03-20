66d4e766eaa5b51fe1f93fdb06f8af4e80117954
Dsotofuya <dsotofuya@gmail.com>
2022-03-20 15:18:14 -0500

    Agregado div de descuento historico

--- src/routes/gameInfo.js
+++ src/routes/gameInfo.js
@@ -6,10 +6,11 @@ router.get("/:gameId", async (req, res) => {
   const gameId = req.params.gameId;
   console.log(`The gameId: ${gameId} has been requested`);
   await db.query(
-    "SELECT * FROM JUEGOS WHERE ID_VIDEOJUEGO = ?",
+    "SELECT * FROM JUEGOS j, HISTORIAL_JUEGOS hj WHERE j.ID_VIDEOJUEGO = hj.ID_VIDEOJUEGO AND j.ID_VIDEOJUEGO = ?",
     [gameId],
     function (err, gameInfo, fields) {
       if (err) throw err;
+      console.log(gameInfo)
       res.render("links/gameInfo", { gameInfo });
     }
   );
