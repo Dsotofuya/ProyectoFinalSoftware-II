7de5b8d09f6f680d15f76a9891d449b110fd21f6
SergioV19 <sergiovar19@gmail.com>
2022-03-21 16:14:19 -0500

    implementando la validacion de correo

--- src/routes/register.js
+++ src/routes/register.js
@@ -4,30 +4,6 @@ const res = require("express/lib/response");
 const router = express.Router();
 const db = require("../database");
 
-// function verificarPasswords() {
-//   pass1 = res.getElementById(CONTRASENA);
-//   pass2 = res.getElementById(CONFIRMATION);
-
-//   // Verificamos si las constraseñas no coinciden
-//   if (pass1.value != pass2.value) {
-//     // Si las constraseñas no coinciden mostramos un mensaje
-//     res.getElementById("error").classList.add("mostrar");
-//     res.render("links/register");
-//   }
-//   else {
-
-//     // Si las contraseñas coinciden ocultamos el mensaje de error
-//     document.getElementById("error").classList.remove("mostrar");
-
-//     // Mostramos un mensaje mencionando que las Contraseñas coinciden
-//     document.getElementById("ok").classList.remove("ocultar");
-
-//     // Desabilitamos el botón de login
-//     document.getElementById("login").disabled = true;
-//     return true;
-//   }
-// }
-
 router.get("/", (req, res) => {
   //res.send("Acá irá la pgina de registro de usuario")
   res.render("links/register");
@@ -40,20 +16,25 @@ router.post("/", async (req, res) => {
   const { NOMBRE, CORREO, PAIS, FECHA_NACIMIENTO, CONTRASENA, CONFIRMATION } = req.body;
 
 
+  await db.query("SELECT CORREO FROM USUARIOS", function (err, mails, fields) {
+    if (err) throw err;
+
+    let error2 = { error: "mostrar" }
+
+    if (validate(mails, CORREO)) {
+      res.render("links/register", { error2 });
+    }
+  });
+
   if (CONTRASENA != CONFIRMATION) {
     // render,
     let error = { error: "mostrar" }
     res.render("links/register", { error })
 
-  } else {
+  }
 
-    await db.query("SELECT CORREO FROM USUARIOS", function (err, mails, fields) {
-      if (err) throw err;
+  else {
 
-      if (validate(mails, CORREO)) {
-        res.render("links/register", { error2 });
-      }
-    });
     const newUser = {
       NOMBRE,
       CORREO,
