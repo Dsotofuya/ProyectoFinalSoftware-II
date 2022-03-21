ff8b63ae93aae55b4b76ebdc3bace81ca5f5253a
Dsotofuya <dsotofuya@gmail.com>
2022-03-21 16:17:48 -0500

    intento de corrección

--- src/routes/register.js
+++ src/routes/register.js
@@ -5,55 +5,52 @@ const router = express.Router();
 const db = require("../database");
 
 router.get("/", (req, res) => {
-  //res.send("Acá irá la pgina de registro de usuario")
   res.render("links/register");
 });
 
-
-
 router.post("/", async (req, res) => {
-
-  const { NOMBRE, CORREO, PAIS, FECHA_NACIMIENTO, CONTRASENA, CONFIRMATION } = req.body;
-
+  const { NOMBRE, CORREO, PAIS, FECHA_NACIMIENTO, CONTRASENA, CONFIRMATION } =
+    req.body;
 
   await db.query("SELECT CORREO FROM USUARIOS", function (err, mails, fields) {
     if (err) throw err;
-
-    let error2 = { error: "mostrar" }
-
+    let error2 = { error: "mostrar" };
     if (validate(mails, CORREO)) {
       res.render("links/register", { error2 });
+    } else {
+      if (CONTRASENA != CONFIRMATION) {
+        // render,
+        let error = { error: "mostrar" }
+        res.render("links/register", { error })
+    
+      }
+      else {
+          const newUser = {
+          NOMBRE,
+          CORREO,
+          PAIS,
+          FECHA_NACIMIENTO,
+          CONTRASENA
+        };
+        console.log(newUser);
+        await db.query("INSERT INTO USUARIOS set ?", [newUser], function (err, result, fields) {
+    
+          if (err) throw err;
+          console.log(result);
+        });
+        res.redirect("/mainPage");
+      }
     }
   });
-
-  if (CONTRASENA != CONFIRMATION) {
-    // render,
-    let error = { error: "mostrar" }
-    res.render("links/register", { error })
-
-  }
-
-  else {
-
-    const newUser = {
-      NOMBRE,
-      CORREO,
-      PAIS,
-      FECHA_NACIMIENTO,
-      CONTRASENA
-    };
-    console.log(newUser);
-    //db.query("SHOW TABLES", function (err, result, fields) {
-    await db.query("INSERT INTO USUARIOS set ?", [newUser], function (err, result, fields) {
-
-      if (err) throw err;
-      console.log(result);
-    });
-    res.redirect("/mainPage");
-  }
 });
 
-function validate(mails, newMail) { for (const mail of mails) { if (mail.CORREO == newMail) { return true; } } return false; }
-
+function validate(mails, newMail) {
+  for (const mail of mails) {
+    if (mail.CORREO == newMail) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = router;
