67f25dc3f4e7f3100a1def5079b02f047eb8529d
Dsotofuya <dsotofuya@gmail.com>
2022-03-21 19:53:05 -0500

    Funcionamiento eliminar y borrar (pendiente mejorar consulta)

--- src/routes/wishlist.js
+++ src/routes/wishlist.js
@@ -4,10 +4,34 @@ const db = require('../database');
 
 router.get("/", async (req, res) => {
   await db.query(
-    "SELECT ID_VIDEOJUEGO, NOMBRE_VIDEOJUEGO, URL_IMAGEN FROM JUEGOS",
+    "SELECT j.ID_VIDEOJUEGO, j.NOMBRE_VIDEOJUEGO, j.URL_IMAGEN, ld.NOTIFICACION FROM JUEGOS j, LISTAS_DESEOS ld WHERE j.ID_VIDEOJUEGO =  ld.ID_VIDEOJUEGO",
     function (err, gameInfo, fields) {
       if (err) throw err;
-      res.render("links/wishList",  { gameInfo });
+      res.render("links/wishList", { gameInfo });
+    }
+  );
+});
+
+router.get("/delete/:gameId", async (req, res) => {
+  let idTemp = req.params
+  console.log("borrando el juego de la lista de deseos : ", idTemp);
+  await db.query(
+    "DELETE FROM LISTAS_DESEOS WHERE ID_VIDEOJUEGO = ?", [idTemp.gameId],
+    function (err, gameInfo, fields) {
+      if (err) throw err;
+      res.redirect("/wishList");
+    }
+  );
+});
+
+router.get("/notify/:gameId", async (req, res) => {
+  let idTemp = req.params
+  console.log(idTemp);
+  await db.query(
+    "UPDATE `LISTAS_DESEOS` SET `NOTIFICACION`= false WHERE `ID_USUARIO` = 1 AND `ID_VIDEOJUEGO` = ?", [idTemp.gameId],
+    function (err, fields) {
+      if (err) throw err;
+      res.redirect("/wishList");
     }
   );
 });
