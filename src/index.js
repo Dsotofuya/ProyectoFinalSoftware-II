4d55b4424a0b386c784cd9e741ddb0ca8613b912
Dsotofuya <dsotofuya@gmail.com>
2022-03-21 21:21:33 -0500

    Agregados modulos requeridos para el manejo de sesiones de usuario

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
 const path = require("path");
+const session = require('express-session');
+const mysqlStore = require('express-mysql-session');
+const { database } = require('./keys')
 
 //Inicializaciones
 const server = express();
@@ -14,22 +17,30 @@ server.set("port", process.env.PORT || 4445);
 server.set("views", path.join(__dirname, "views"));
 //Se define la configuración para handlebars
 server.engine(".hbs", engine({
-    //se escoje el nombre de la pagina default que estará en la carpeta views y subcarpeta layouts
-    defaultlayout: "main",
-    //Se concatena la ruta de la carpeta views con la carpeta layouts
-    layoutsDir: path.join(server.get("views"), "layouts"),
-    partialDir: path.join(server.get("views"), "partials"),
-    //Se define que la extención de los archivos no será ".handlebars", si no hbs
-    extname: ".hbs",
-    //Como handlebars requiere hacer procesos por aparte se referirá a que hará todo en el archivo handlebars en la carpeta lib
-    helpers: require("./lib/handlebars"),
-  })
+  //se escoje el nombre de la pagina default que estará en la carpeta views y subcarpeta layouts
+  defaultlayout: "main",
+  //Se concatena la ruta de la carpeta views con la carpeta layouts
+  layoutsDir: path.join(server.get("views"), "layouts"),
+  partialDir: path.join(server.get("views"), "partials"),
+  //Se define que la extención de los archivos no será ".handlebars", si no hbs
+  extname: ".hbs",
+  //Como handlebars requiere hacer procesos por aparte se referirá a que hará todo en el archivo handlebars en la carpeta lib
+  helpers: require("./lib/handlebars"),
+})
 );
 //Se le dice al servidor que usará el view engine de handlebars que se configuró arriba (linea16-27)
 server.set("view engine", ".hbs");
 
 
 //Middlewares
+// Administrador de sesiones
+server.use(session({
+  secret: 'appMySqlNodeSession',
+  resave: false,
+  saveUninitialized: false,
+  store: new mysqlStore(database)
+}
+))
 server.use(morgan("short"));
 //Este metodo es para indiciar que solo se recibiran datos en formatos string, nada de imagenes
 server.use(express.urlencoded({ extended: false }));
