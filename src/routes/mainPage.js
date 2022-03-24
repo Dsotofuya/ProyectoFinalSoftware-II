1cf9c19f60a3f6c9cb71b4a5bbb4615e3bf0554e
Dsotofuya <dsotofuya@gmail.com>
2022-03-24 03:34:22 -0500

    Main page dinamico pero no guarda datos en la variable

--- src/routes/mainPage.js
+++ src/routes/mainPage.js
@@ -6,19 +6,49 @@ router.get("/", async (req, res) => {
   const user = req.user
   if (user == undefined) {
     await db.query(
-      "SELECT ID_VIDEOJUEGO, NOMBRE_VIDEOJUEGO, URL_IMAGEN FROM JUEGOS LIMIT 0, 5",
+      "SELECT ID_VIDEOJUEGO, NOMBRE_VIDEOJUEGO, URL_IMAGEN FROM `JUEGOS` ORDER BY `JUEGOS`.`POPULARIDAD` DESC LIMIT 0,5",
       function (err, gameInfo, fields) {
         if (err) throw err;
         res.render("links/mainPage", { gameInfo });
       }
     );
   } else {
+    let dataUser = {}
     await db.query(
-      "SELECT ID_VIDEOJUEGO, NOMBRE_VIDEOJUEGO, URL_IMAGEN FROM JUEGOS LIMIT 0, 5",
-      function (err, gameInfo, fields) {
+      "SELECT ID_VIDEOJUEGO, NOMBRE_VIDEOJUEGO, URL_IMAGEN FROM `JUEGOS` ORDER BY `JUEGOS`.`POPULARIDAD` DESC LIMIT 0,5",
+      function (err, gamepopular) {
         if (err) throw err;
-        res.render("links/mainPage", { gameInfo });
+        console.log(gamepopular);
+        // dataUser.popular = gamepopular
+        // return dataUser;
       })
+      // console.log(a)
+    await db.query("SELECT gj.ID_GENERO, g.NOMBRE_GENERO , gj.FRECUENCIA FROM `GENEROS_USUARIOS` gj, GENEROS g WHERE gj.ID_GENERO = g.ID_GENERO AND `ID_USUARIO` = ?",
+      [user.ID_USUARIO], async function (err, genreExist) {
+        if (err) throw err;
+        const isEmpty = Object.keys(genreExist).length === 0;
+        if (isEmpty) {
+          res.render("links/mainPage", { dataUser });
+        } else {
+          for (const genre of genreExist) {
+            dataUser.genres = genre;
+            await db.query(
+              "SELECT j.ID_VIDEOJUEGO, j.NOMBRE_VIDEOJUEGO, j.URL_IMAGEN FROM JUEGOS j , GENEROS_JUEGOS gj WHERE j.ID_VIDEOJUEGO = gj.ID_VIDEOJUEGO AND gj.ID_GENERO = ? LIMIT 0, 5;", [genre.ID_GENERO], function (errr, gameInfo) {
+                if (errr) throw err;
+                // dataUser.genre = genre;
+                console.log(genre)
+                for (const game of gameInfo) {
+                  console.log(game)
+                }
+                // res.render("links/mainPage", { gameInfo });
+              }
+            )
+          }
+        }
+      })
+    console.log(user.ID_USUARIO)
+    console.log(dataUser)
+    res.send("xd")
   }
 });
 
