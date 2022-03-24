3362c17c7c5376989ddb4d6addbd629eed5cc802
Dsotofuya <dsotofuya@gmail.com>
2022-03-23 23:55:59 -0500

    Correción y mejora de querys para consulta de un juego

--- src/routes/gameInfo.js
+++ src/routes/gameInfo.js
@@ -20,7 +20,15 @@ router.get("/:gameId", async (req, res) => {
       let gameInfo = await consultGame(gameId)
       res.render("links/gameInfo", { gameInfo });
     } else {
-      // res.send("xd")
+      await updateGame(gameId)
+      await updateGameHistory(gameId)
+      await db.query("SELECT * FROM JUEGOS j, HISTORIAL_JUEGOS hj WHERE j.ID_VIDEOJUEGO = hj.ID_VIDEOJUEGO AND j.ID_VIDEOJUEGO = ?", [gameId],
+        async function (error, gameInfo) {
+          if (error)
+            throw error;
+          res.render("links/gameInfo", { gameInfo });
+        }
+      );
     }
   })
 });
@@ -107,42 +115,33 @@ async function consultGame(gameId) {
   );
 }
 
+async function updateGame(gameId) {
+  const updatedGame = await scrapingManager.scrapGameById(gameId);
+  await db.query("UPDATE `JUEGOS` SET `PRECIO_STEAM`= ?,`PRECIO_EPIC`= ? ,`PRECIO_ENEBA`= ?,POPULARIDAD=POPULARIDAD+1 WHERE `ID_VIDEOJUEGO` = ?", [updatedGame.PRECIO_STEAM, updatedGame.
+    PRECIO_EPIC, updatedGame.PRECIO_ENEBA, gameId])
+}
 
-//  await updateGame(NOMBRE_VIDEOJUEGO, gameId, res);
-// // res.send("XD")
-
-// await updateGame(gameExist[0].NOMBRE_VIDEOJUEGO, gameId, res);
-
-// async function updateGame(gameName, gameId, res) {
-//   const updatedGame = await scrapingManager.scrapGame(gameName);
-//   await db.query("UPDATE `JUEGOS` SET `PRECIO_STEAM`= ?,`PRECIO_EPIC`= ? ,`PRECIO_ENEBA`= ?,POPULARIDAD=POPULARIDAD+1 WHERE `ID_VIDEOJUEGO` = ?", [updatedGame.PRECIO_STEAM, updatedGame.PRECIO_EPIC, updatedGame.PRECIO_ENEBA, gameId]);
-//   await db.query("SELECT PRECIO FROM HISTORIAL_JUEGOS WHERE ID_VIDEOJUEGO  = ?", [gameId], async function (err3, lowerActualPrice) {
-//     if (err3) throw err3;
-//     await db.query(
-//       "SELECT NOMBRE_VIDEOJUEGO, PRECIO_STEAM, CASE WHEN PRECIO_EPIC = -1 THEN 99999 ELSE PRECIO_EPIC END PRECIO_EPIC_FORMATED, CASE WHEN PRECIO_ENEBA = -1 THEN 99999 ELSE PRECIO_ENEBA END PRECIO_ENEBA_FORMATED FROM JUEGOS  WHERE ID_VIDEOJUEGO = ?",
-//       [gameId],
-//       async function (err2, result) {
-//         if (err2)
-//           throw err2;
-//         const minPrice = Math.min(result[0].PRECIO_STEAM, result[0].PRECIO_EPIC_FORMATED, result[0].PRECIO_ENEBA_FORMATED, lowerActualPrice[0].PRECIO);
-//         if (result[0].PRECIO_STEAM == minPrice) {
-//           await db.query("UPDATE `HISTORIAL_JUEGOS` SET TIENDA`= ?, `PRECIO`= ? WHERE `ID_VIDEOJUEGO` = ?", ["Steam", minPrice, gameId]);
-//         } else if (result[0].PRECIO_EPIC_FORMATED == minPrice) {
-//           await db.query("UPDATE `HISTORIAL_JUEGOS` SET TIENDA`= ?, `PRECIO`= ? WHERE `ID_VIDEOJUEGO` = ?", ["Epic Store", minPrice, gameId]);
-//         } else if (result[0].PRECIO_ENEBA_FORMATED == minPrice) {
-//           await db.query("UPDATE `HISTORIAL_JUEGOS` SET TIENDA`= ?, `PRECIO`= ? WHERE `ID_VIDEOJUEGO` = ?", ["Eneba", minPrice, gameId]);
-//         } else if (lowerActualPrice[0].PRECIO == minPrice) {
-//           console.log("historico del juego ", gameId, " no ha cambiado");
-//         }
-//         await db.query("SELECT * FROM JUEGOS j, HISTORIAL_JUEGOS hj WHERE j.ID_VIDEOJUEGO = hj.ID_VIDEOJUEGO AND j.ID_VIDEOJUEGO = ?", [gameId],
-//           async function (error, gameInfo) {
-//             if (error)
-//               throw error;
-//             res.render("links/gameInfo", { gameInfo });
-//           }
-//         );
-//       });
-//   });
-// }
+async function updateGameHistory(gameId) {
+  await db.query("SELECT PRECIO FROM HISTORIAL_JUEGOS WHERE ID_VIDEOJUEGO  = ?",
+    [gameId], async function (err, lowerActualPrice) {
+      if (err) throw err;
+      await db.query(
+        "SELECT NOMBRE_VIDEOJUEGO, PRECIO_STEAM, CASE WHEN PRECIO_EPIC = -1 THEN 99999 ELSE PRECIO_EPIC END PRECIO_EPIC_FORMATED, CASE WHEN PRECIO_ENEBA = -1 THEN 99999 ELSE PRECIO_ENEBA END PRECIO_ENEBA_FORMATED FROM JUEGOS  WHERE ID_VIDEOJUEGO = ?", [gameId],
+        async function (err2, result) {
+          if (err2) throw err2;
+          const minPrice = Math.min(result[0].PRECIO_STEAM, result[0].PRECIO_EPIC_FORMATED, result[0].PRECIO_ENEBA_FORMATED, lowerActualPrice[0].PRECIO);
+          if (result[0].PRECIO_STEAM == minPrice) {
+            await db.query("UPDATE `HISTORIAL_JUEGOS` SET TIENDA`= ?, `PRECIO`= ? WHERE `ID_VIDEOJUEGO` = ?", ["Steam", minPrice, gameId]);
+          } else if (result[0].PRECIO_EPIC_FORMATED == minPrice) {
+            await db.query("UPDATE `HISTORIAL_JUEGOS` SET TIENDA`= ?, `PRECIO`= ? WHERE `ID_VIDEOJUEGO` = ?", ["Epic Store", minPrice, gameId]);
+          } else if (result[0].PRECIO_ENEBA_FORMATED == minPrice) {
+            await db.query("UPDATE `HISTORIAL_JUEGOS` SET TIENDA`= ?, `PRECIO`= ? WHERE `ID_VIDEOJUEGO` = ?", ["Eneba", minPrice, gameId]);
+          } else if (lowerActualPrice[0].PRECIO == minPrice) {
+            console.log("historico del juego ", gameId, " no ha cambiado");
+          }
+        }
+      )
+    })
+}
 
 module.exports = router;
